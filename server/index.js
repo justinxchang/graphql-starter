37c5528b56acd588f0f1beafae4608d0faba6139
Justin Chang <justinxchang@hotmail.com>
2018-12-06 15:32:22 -0700

    first commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,35 @@
 
+const { GraphQLServer } = require('graphql-yoga')
+
+const resolvers = require('./schema/resolvers')
+
+const options = {
+    port: 3050,
+    endpoint: '/graphql',
+    playground: '/graphiql'
+}
+
+const server = new GraphQLServer({
+    resolvers,
+    typeDefs: 'server/schema/typeDefs.graphql',
+    context: (req) => {
+        return {
+            ...req.request
+        }
+    }
+})
+
+server.start(options, () => {                   //basically like app.listen()
+    console.log(`Running on port: ${options.port}`)
+})
+
+
+
+
+
+
+
+
+
+
+// server.middlewares.someMiddleware = expressSession({})    look this up if you're interested in sessions, not needed for this
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,1 +1,30 @@
-const products = require('../model');
+const productItems = require('../model');
+
+const products = (parent, args, ctx) => {
+    return productItems
+}
+
+const product = (parent, args) => {
+    const item = productItems.find((val) => {
+        return val.id === parseInt(args.id)
+    });
+    if(!item) {
+        throw new Error(`No product matching ID: ${args.id}`)
+    }
+    return item
+}
+
+const addProduct = (parent, args) => {
+    productItems.push(args)
+    return productItems
+}
+
+module.exports = {
+    Query: {
+        products,
+        product
+    },
+    Mutation: {
+        addProduct
+    }
+}
